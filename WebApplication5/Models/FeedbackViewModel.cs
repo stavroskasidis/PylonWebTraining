d89ac9ddb2b227fb165c39bee8af92f8a467643f
Stavros Kasidis <s.kasidis@epsilonnet.gr>
2019-06-19 17:39:53 +0300

    init commit

---
+++ WebApplication5/Models/FeedbackViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class FeedbackViewModel
+    {
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string FeedbackText { get; set; }
+    }
+}
