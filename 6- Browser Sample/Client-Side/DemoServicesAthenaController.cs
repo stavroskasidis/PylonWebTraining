b932eb86bc4357cbb755ee56923ee8516402b86d
Stavros Kasidis <s.kasidis@epsilonnet.gr>
2019-07-04 17:05:27 +0300

    added 6 sample code

---
+++ 6- Browser Sample/Client-Side/DemoServicesAthenaController.cs
@@ -0,0 +1,24 @@
+﻿using Hercules.Libs.Web.Client.ControllerOptions;
+using Hercules.Libs.Web.Client.Controllers;
+using Poseidon.Libs.Web.Client.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hercules.ATH.Web.Masters.Items
+{
+    [powAuthorizeActions(typeof(hewServicesAthenaAction))]
+    public class hewDemoServicesAthenaController : hewBrowserController
+    {
+        protected override string BrowserDomain => "Hercules";
+
+        protected override string BrowserName => "Demo.Services.Athena";
+
+        protected override void SetPageOptions(hewPageOptionsBuilder builder)
+        {
+            builder.Title("Services Demo");
+        }
+    }
+}
---
+++ 6- Browser Sample/Client-Side/DemoServicesAthenaQuickFilters.cs
@@ -0,0 +1,28 @@
+﻿using Poseidon.Libs.Web.Client;
+using Poseidon.Libs.Web.UI;
+using Poseidon.Libs.Web.UI.Client.ViewEngine.SystemViews;
+using Poseidon.Libs.Web.UI.Client.ViewEngine.ViewModels;
+using Poseidon.Libs.Web.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hercules.ATH.Web.Masters.Items
+{
+    public class hewDemoServicesAthenaQuickFilters : powQuickFiltersPartial
+    {
+        protected override void BuildQuickFiltersForm(powFormBuilder<powBrowserQuickFiltersViewModel> form)
+        {
+            form.AddPanel("ItemsFilters", panel =>
+            {
+                panel.HeaderCaption(hewResources.sQuickFilters);
+                panel.AddStackLayout(powStackLayoutOrientation.Vertical, stack =>
+                {
+                    stack.AddTextBox("Name").Caption(hewResources.sName);
+                });
+            });
+        }
+    }
+}
