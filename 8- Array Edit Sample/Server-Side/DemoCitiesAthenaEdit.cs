13e84c3c9650d736763c5d940fed575905be8478
Stavros Kasidis <s.kasidis@epsilonnet.gr>
2019-07-16 10:14:13 +0300

    Added 8

---
+++ 8- Array Edit Sample/Server-Side/DemoCitiesAthenaEdit.cs
@@ -0,0 +1,28 @@
+﻿using Hercules.Libs.Web.Server.Views;
+using Hercules.Obj.Web.INF.DataObjectProxies.Geography;
+using Poseidon.Libs.Web.Base;
+using Poseidon.Libs.Web.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hercules.ATH.Obj.Web.Arrays.Miscellaneous
+{
+    [powSchemaView("Hercules", "Demo.Cities.Athena.Edit")]
+    public class hewDemoCitiesAthenaEdit : hewArrayEditView<hewCitiesDataObjectProxy>
+    {
+        protected override string Name => "Cities";
+        protected override string Controller => "hewDemoCitiesAthenaEdit";
+
+        protected override void BuildGrid(powEditableGridBuilder<hewCitiesDataObjectProxy> gridBuilder)
+        {
+            base.BuildGrid(gridBuilder);
+            gridBuilder.AddColumnFor(model => model.PrfcID, hewResources.sPrefecture, 
+                col => col.SetArrayLookUpEditor("Hercules", "PrefecturesAthenaArrayLookUpDataSource"));
+            gridBuilder.AddColumnFor(model => model.MncpID, hewResources.sMunicipality, 
+                col => col.SetArrayLookUpEditor("Hercules", "MunicipalitiesAthenaArrayLookUpDataSource"));
+        }
+    }
+}
